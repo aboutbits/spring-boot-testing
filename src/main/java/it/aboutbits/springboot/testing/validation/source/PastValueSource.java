5c9c3364910d800f8b5d93d1655ef9590d21e061
Thomas Sapelza <thomas.sapelza@aboutbits.it>
2025-01-10 12:03:33 +0100

    add Temporal types Instant, LocalTime, OffsetTime, Year, YearMonth and ZonedDateTime

--- src/main/java/it/aboutbits/springboot/testing/validation/source/PastValueSource.java
+++ src/main/java/it/aboutbits/springboot/testing/validation/source/PastValueSource.java
@@ -2,10 +2,17 @@ package it.aboutbits.springboot.testing.validation.source;
 
 import it.aboutbits.springboot.testing.validation.core.ValueSource;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.Year;
+import java.time.YearMonth;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -14,10 +21,17 @@ import java.util.stream.Stream;
 public class PastValueSource implements ValueSource {
     private static final Map<Class<?>, Function<Object[], Stream<?>>> TYPE_SOURCES = new HashMap<>();
 
+    // https://jakarta.ee/specifications/bean-validation/3.0/apidocs/jakarta/validation/constraints/past
     static {
+        TYPE_SOURCES.put(Instant.class, PastValueSource::getInstantStream);
+        TYPE_SOURCES.put(LocalTime.class, PastValueSource::getLocalTimeStream);
         TYPE_SOURCES.put(LocalDate.class, PastValueSource::getLocalDateStream);
-        TYPE_SOURCES.put(LocalDateTime.class, PastValueSource::getLocalDatetimeStream);
+        TYPE_SOURCES.put(LocalDateTime.class, PastValueSource::getLocalDateTimeStream);
+        TYPE_SOURCES.put(OffsetTime.class, PastValueSource::getOffsetTimeStream);
         TYPE_SOURCES.put(OffsetDateTime.class, PastValueSource::getOffsetDateTimeStream);
+        TYPE_SOURCES.put(Year.class, PastValueSource::getYearStream);
+        TYPE_SOURCES.put(YearMonth.class, PastValueSource::getYearMonthStream);
+        TYPE_SOURCES.put(ZonedDateTime.class, PastValueSource::getZonedDateTimeStream);
     }
 
     @SuppressWarnings("unused")
@@ -36,25 +50,66 @@ public class PastValueSource implements ValueSource {
         throw new IllegalArgumentException("Property class not supported!");
     }
 
+    private static Stream<Instant> getInstantStream(Object[] args) {
+        var currentInstant = Instant.now();
+        var smallestInstant = Instant.MIN;
+
+        return Stream.of(smallestInstant, currentInstant.minusSeconds(1L));
+    }
+
+    private static Stream<LocalTime> getLocalTimeStream(Object[] args) {
+        var currentLocalTime = LocalTime.now();
+        var smallestLocalTime = LocalTime.MIN;
+
+        return Stream.of(smallestLocalTime, currentLocalTime.minusSeconds(1L));
+    }
 
     private static Stream<LocalDate> getLocalDateStream(Object[] args) {
         var currentDate = LocalDate.now();
         var smallestDate = LocalDate.MIN;
 
-        return Stream.of(smallestDate, currentDate.minusDays(1));
+        return Stream.of(smallestDate, currentDate.minusDays(1L));
     }
 
-    private static Stream<LocalDateTime> getLocalDatetimeStream(Object[] args) {
+    private static Stream<LocalDateTime> getLocalDateTimeStream(Object[] args) {
         var currentDateTime = LocalDateTime.now();
         var smallestDateTime = LocalDateTime.MIN;
 
-        return Stream.of(smallestDateTime, currentDateTime.minusSeconds(1));
+        return Stream.of(smallestDateTime, currentDateTime.minusSeconds(1L));
+    }
+
+    private static Stream<OffsetTime> getOffsetTimeStream(Object[] args) {
+        var currentOffsetTime = OffsetTime.now(ZoneOffset.UTC);
+        var smallestOffsetTime = OffsetTime.MIN;
+
+        return Stream.of(smallestOffsetTime, currentOffsetTime.minusSeconds(1L));
     }
 
     private static Stream<OffsetDateTime> getOffsetDateTimeStream(Object[] args) {
         var currentOffsetDateTime = OffsetDateTime.now(ZoneOffset.UTC);
         var smallestOffsetDateTime = OffsetDateTime.MIN;
 
-        return Stream.of(smallestOffsetDateTime, currentOffsetDateTime.minusSeconds(1));
+        return Stream.of(smallestOffsetDateTime, currentOffsetDateTime.minusSeconds(1L));
+    }
+
+    private static Stream<Year> getYearStream(Object[] args) {
+        var currentYear = Year.now();
+        var smallestYear = Year.of(Year.MIN_VALUE);
+
+        return Stream.of(smallestYear, currentYear.minusYears(1L));
+    }
+
+    private static Stream<YearMonth> getYearMonthStream(Object[] args) {
+        var currentYearMonth = YearMonth.now();
+        var smallestYearMonth = YearMonth.of(Year.MIN_VALUE, Month.JANUARY);
+
+        return Stream.of(smallestYearMonth, currentYearMonth.minusMonths(1L));
+    }
+
+    private static Stream<ZonedDateTime> getZonedDateTimeStream(Object[] args) {
+        var currentZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC);
+        var smallestZonedDateTime = LocalDateTime.MIN.atZone(ZoneOffset.MAX);
+
+        return Stream.of(smallestZonedDateTime, currentZonedDateTime.minusSeconds(1L));
     }
 }
