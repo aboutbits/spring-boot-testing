54a80a7db07735691ebeb9899f75ff6e2749a75c
Andreas Hufler <andreas.hufler@aboutbits.it>
2025-12-15 15:47:12 +0100

    spring boot 4 update

--- src/main/java/it/aboutbits/springboot/testing/web/request/HttpTestClient.java
+++ src/main/java/it/aboutbits/springboot/testing/web/request/HttpTestClient.java
@@ -1,45 +1,45 @@
 package it.aboutbits.springboot.testing.web.request;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import tools.jackson.databind.json.JsonMapper;
 
 @RequiredArgsConstructor
 public class HttpTestClient {
     private final MockMvc mockMvc;
-    private final ObjectMapper objectMapper;
+    private final JsonMapper jsonMapper;
 
     public Request<MockHttpServletRequestBuilder> get(@NonNull String url, Object... pathVariables) {
-        return new Get(mockMvc, objectMapper, url, pathVariables);
+        return new Get(mockMvc, jsonMapper, url, pathVariables);
     }
 
     public Request<MockHttpServletRequestBuilder> put(@NonNull String url, Object... pathVariables) {
-        return new Put(mockMvc, objectMapper, url, pathVariables);
+        return new Put(mockMvc, jsonMapper, url, pathVariables);
     }
 
     public MultipartRequest putMultipart(@NonNull String url, Object... pathVariables) {
-        return new PutMultipart(mockMvc, objectMapper, url, pathVariables);
+        return new PutMultipart(mockMvc, jsonMapper, url, pathVariables);
     }
 
     public Request<MockHttpServletRequestBuilder> post(@NonNull String url, Object... pathVariables) {
-        return new Post(mockMvc, objectMapper, url, pathVariables);
+        return new Post(mockMvc, jsonMapper, url, pathVariables);
     }
 
     public MultipartRequest postMultipart(@NonNull String url, Object... pathVariables) {
-        return new PostMultipart(mockMvc, objectMapper, url, pathVariables);
+        return new PostMultipart(mockMvc, jsonMapper, url, pathVariables);
     }
 
     public Request<MockHttpServletRequestBuilder> patch(@NonNull String url, Object... pathVariables) {
-        return new Patch(mockMvc, objectMapper, url, pathVariables);
+        return new Patch(mockMvc, jsonMapper, url, pathVariables);
     }
 
     public MultipartRequest patchMultipart(@NonNull String url, Object... pathVariables) {
-        return new PatchMultipart(mockMvc, objectMapper, url, pathVariables);
+        return new PatchMultipart(mockMvc, jsonMapper, url, pathVariables);
     }
 
     public Request<MockHttpServletRequestBuilder> delete(@NonNull String url, Object... pathVariables) {
-        return new Delete(mockMvc, objectMapper, url, pathVariables);
+        return new Delete(mockMvc, jsonMapper, url, pathVariables);
     }
 }
