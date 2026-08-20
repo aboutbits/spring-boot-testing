95a4404746e7c71c6ea97d09ca2a6b2b58bbcb5b
Peter Moser <peter.moser@aboutbits.it>
2026-08-20 13:17:48 +0200

    add email validation

---
+++ src/main/java/it/aboutbits/springboot/testing/validation/rule/EmailRule.java
@@ -0,0 +1,24 @@
+package it.aboutbits.springboot.testing.validation.rule;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+import it.aboutbits.springboot.testing.validation.core.BaseRuleBuilder;
+import it.aboutbits.springboot.testing.validation.core.Rule;
+import it.aboutbits.springboot.testing.validation.core.ValidationRulesData;
+import it.aboutbits.springboot.testing.validation.source.InvalidEmailValueSource;
+import org.jspecify.annotations.NullMarked;
+
+@SuppressWarnings("unchecked")
+@NullMarked
+public interface EmailRule<V extends BaseRuleBuilder<?>> extends ValidationRulesData {
+    /**
+     * Bean validation considers `null` and blank values to be valid e-mail addresses.
+     * Combine this rule with `notNull` or `notBlank` if the property must be present.
+     */
+    @CheckReturnValue
+    default V email(String property) {
+        addRule(
+                new Rule(property, InvalidEmailValueSource.class)
+        );
+        return (V) this;
+    }
+}
